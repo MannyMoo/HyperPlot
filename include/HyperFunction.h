e59708fb4478943fd4570cfa0812115f7270895e
Sam Harnew <sam.harnew@gmail.com>
2016-10-13 22:06:40 +0100

    Add functions to make plotting slices of a hyper function easy

--- include/HyperFunction.h
+++ include/HyperFunction.h
@@ -15,8 +15,11 @@
 // HyperPlot includes
 #include "MessageService.h"
 #include "HyperPointSet.h"
+#include "HyperCuboid.h"
+#include "RootPlotter2D.h"
 
 // Root includes
+#include "TH2D.h"
 
 // std includes
 
@@ -24,14 +27,31 @@
 class HyperFunction{  
 
   private:
+  
+  HyperCuboid _limits;
 
   public:
 
-  HyperFunction(){;} /**< Constructor */
+  HyperFunction(); /**< Constructor */
+  HyperFunction(const HyperCuboid& limits); /**< Constructor */
 
   virtual double getVal(const HyperPoint& point) const = 0; /**< Virtual function that defines a HyperFunction (Map from HyperPoint -> double) */
   
   void reweightDataset(HyperPointSet& points);
+  
+  void setFuncLimits(const HyperCuboid& limits);
+
+  TH2D make2DFuncSlice   (TString name, int sliceDimX, int sliceDimY, const HyperPoint& slicePoint, int nbins) const;
+  void draw2DFuncSlice   (TString path, int sliceDimX, int sliceDimY, const HyperPoint& slicePoint, int nbins) const;
+  void draw2DFuncSliceSet(TString path, int sliceDimX, int sliceDimY, int sliceSetDim, int nSlices, const HyperPoint& slicePoint, int nbins) const;
+  void draw2DFuncSliceSet(TString path, int sliceDimX, int sliceDimY, int nSlices, const HyperPoint& slicePoint, int nbins) const;
+  void draw2DFuncSliceSet(TString path, int nSlices, const HyperPoint& slicePoint, int nbins) const;
+  
+  double getDifference(const HyperFunction& other, const HyperPoint& point);
+  /**< Get the difference between this HyperFunction and another HyperFunction at a point
+  in n-dim space. (diff = this - other) */
+
+  void fillCorrelations(TH2D& hist, const HyperFunction& other, const HyperPointSet& points);
 
   virtual ~HyperFunction(){;} /**< Destructor */
 
